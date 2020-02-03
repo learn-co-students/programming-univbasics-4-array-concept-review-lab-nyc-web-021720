18882bb0017e8b1931c91ad982a1057ddbb5bbf4
Ryan Vaznis <ryan.vaznis@gmail.com>
2020-02-03 16:21:55 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,17 +1,40 @@
 def find_element_index(array, value_to_find)
+
+#i didn't realize you could just return a 'nil' value standalone my 1st time thru
   
-  found_val = array.index(value_to_find)
+  array.length.times do | index |
+    if array[index] == value_to_find
+      return index
+    end
+  end
   
+  nil
+
 end
 
 def find_max_value(array)
   
-  array.sort.last
+  maxNum = 0
+  
+  array.length.times do | index |
+    if array[index] > maxNum
+      maxNum = array[index]
+    end
+  end
+  
+  maxNum
   
 end
 
 def find_min_value(array)
   
-  array.sort.first
+  minNum = array[0]
   
+  array.length.times do | index |
+    if array[index] < minNum
+      minNum = array[index]
+    end
+  end
+  
+  minNum
 end
